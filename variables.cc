9301ede7083c69ae874e2bbe0e9afeac4123d690
Victoria Brown <victoriabrown0710@gmail.com>
2016-09-28 17:17:23 -0500

    Add files via upload

---
+++ variables.cc
@@ -0,0 +1,25 @@
+/* Operating with variables in C++
+Programmer:     Victoria Brown
+Date:           September 2016
+Variables.cc */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Declaring varaibles:
+  int a, b;
+  int result;
+
+  // Process:
+  a = 5;
+  b = 2;
+  a = a + 1;
+  result = a - b;
+
+  // Print out the result:
+  cout << result;
+
+  // Terminate the program:
+  return 0;
+}
