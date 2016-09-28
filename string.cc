ab8be7485111178044686baf2596d73fb21f8f86
Victoria Brown <victoriabrown0710@gmail.com>
2016-09-28 18:32:05 -0500

    Add file string.cc

---
+++ string.cc
@@ -0,0 +1,17 @@
+/* Playing with Strings
+Programmer:     Victoria Brown
+Date:           September 2016
+strings.cc */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string mystring;
+  mystring = "This is the initial string content";
+  cout << mystring << endl;       // endl -> ends the line
+  mystring = "This is a different string content";
+  cout << mystring << endl;
+  return 0;
+}
