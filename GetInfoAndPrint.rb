9228b80a518c5062decb15301501e36c0f2b3064
Victoria Brown <victoriabrown0710@gmail.com>
2016-10-05 19:18:03 -0500

    Add more comments

--- GetInfoAndPrint.rb
+++ GetInfoAndPrint.rb
@@ -9,12 +9,15 @@ Date:               October 2016
 print "What's your first name?"
 first_name = gets.chomp.capitalize!
 
+# Get user's last name and set it to variable last_name
 print "What's your last name?"
 last_name = gets.chomp.capitalize!
 
+# Get user's city and set it to variable city
 print "What city are you from?"
 city = gets.chomp.capitalize!
 
+# Print out the info I just got with chomp
 print "What state are you from (enter abbreviated format such as TX for Texas)?"
 state = gets.chomp.upcase!
 
