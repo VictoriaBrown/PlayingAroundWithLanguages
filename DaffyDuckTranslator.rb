2fb0f912c07af096ad6a71de0a8c5425fa628fa9
Victoria Brown <victoriabrown0710@gmail.com>
2016-10-05 21:35:22 -0500

    Add comments

--- DaffyDuckTranslator.rb
+++ DaffyDuckTranslator.rb
@@ -5,19 +5,25 @@ Programmer:         Victoria Brown
 Date:               October 2016
 =end
 
+# Get string from user
 print "Please enter what you'd like to be translated to Daffy Duck's tounge: "
 user_input = gets.chomp
 
+# if string has an s
 if user_input.include? "s"
     user_input.gsub!(/s/, "th")
     puts "#{user_input}"
+# if string has an S
 elsif user_input.include? "S"
     user_input.gsub!(/S/, "Th")
     puts "#{user_input}"
+# if string is empty
 elsif user_input == ""
     puts "nothing to replace! you didn't enter anything :'("
+# if string has a sc
 elsif user_input.include? "sc"
     user_input.gsub!(/sc/, "th")
     puts "#{user_input}"
+# no more options! there were no s's in string
 else
     puts "You didn't enter any s's in string! There was nothing to change."
